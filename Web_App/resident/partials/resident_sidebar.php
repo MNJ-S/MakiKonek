babe5fd75232632c449ce48327c5a5a0951cd8c1
ntlpdhl <nataliepaduhilao@gmail.com>
2026-05-30 16:22:08 +0800

    update resident sidebar

--- Web_App/resident/partials/resident_sidebar.php
+++ Web_App/resident/partials/resident_sidebar.php
@@ -9,20 +9,20 @@
             <i class="fa-regular fa-file-lines"></i>
             My Requests
         </a>
-        <a href="#">
+        <a class="<?php echo $activePage === 'reservations' ? 'active' : ''; ?>" href="reservations.php">
             <i class="fa-regular fa-calendar"></i>
             Reservations
         </a>
-        <a href="#">
+        <a class="<?php echo $activePage === 'notifications' ? 'active' : ''; ?>" href="notifications.php">
             <i class="fa-regular fa-bell"></i>
             Notifications
             <span class="count">3</span>
         </a>
-        <a href="#">
+        <a class="<?php echo $activePage === 'profile' ? 'active' : ''; ?>" href="profile.php">
             <i class="fa-regular fa-user"></i>
             Profile
         </a>
-        <a href="#">
+        <a class="<?php echo $activePage === 'settings' ? 'active' : ''; ?>" href="settings.php">
             <i class="fa-solid fa-gear"></i>
             Settings
         </a>
