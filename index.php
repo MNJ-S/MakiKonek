3c007b0a5eb2ea0323afcf33acf1bd9962923565
jhy-atnn <jhodyatinon@gmail.com>
2026-05-30 20:57:03 +0800

    Index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 // Redirect visitors to the public MakiKonek homepage.
-header('Location: Web_App\login_reg.php');
+header('Location: Web_App/public/index.php');
+
+// Web_App\resident\dashboard.php
+// Web_App\login_reg.php
 exit;
