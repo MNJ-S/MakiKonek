52020581d0643c1e17c0e7a82684a4504175039c
jhy-atnn <jhodyatinon@gmail.com>
2026-06-01 15:51:06 +0800

    modified:   Web_App/includes/header.php

--- Web_App/includes/header.php
+++ Web_App/includes/header.php
@@ -3,7 +3,7 @@
 $currentPage = basename($_SERVER['PHP_SELF']);
 $navBase = $navBase ?? '';
 $assetBase = $assetBase ?? '../assets';
-$loginHref = $loginHref ?? '../login_reg.php';
+$loginHref = '/MakiKonek/Web_App/login_reg.php';
 $isResidentHeader = $isResidentHeader ?? false;
 
 // Grab the username from the session if it exists, otherwise fallback to "Resident"
