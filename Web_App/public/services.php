87d726fc678b1132550a48bccd93897e16758011
Shemaiah Ezra <shemaiahezra.magpayo@gmail.com>
2026-06-19 13:38:37 +0800

    Fixed Service Page footer

--- Web_App/public/services.php
+++ Web_App/public/services.php
@@ -291,45 +291,7 @@ $serviceRequestHref = $isResidentHeader ? '../resident/requests.php' : '../login
         </section>
     </main>
 
-    <!-- FIXED SITE FOOTER -->
-    <footer class="site-footer">
-        <div class="footer-grid">
-            <div>
-                <img class="footer-logo" src="../assets/img/logo-makikonek.png" alt="MakiKonek Logo">
-                <p>MakiKonek is your digital gateway to faster, easier, and more transparent barangay service.</p>
-                <div class="social-row">
-                    <a href="#">f</a>
-                    <a href="#">●</a>
-                    <a href="#">☏</a>
-                    <a href="#">✉</a>
-                </div>
-            </div>
-            <div>
-                <h3>Quick Links</h3>
-                <a href="index.php">Home</a>
-                <a href="about.php">About</a>
-                <a href="services.php">Services</a>
-                <a href="announcements.php">Announcements</a>
-                <a href="index.php#contact">Contact</a>
-            </div>
-            <div>
-                <h3>Other Links</h3>
-                <a href="http://localhost/MakiKonek/MakiKonek/Web_App/public/privacy_policy.php" target="_blank" class="text-white text-decoration-none opacity-75">Privacy Policy</a></li>
-                <a href="http://localhost/MakiKonek/MakiKonek/Web_App/public/terms_conditions.php" target="_blank" class="text-white text-decoration-none opacity-75">Terms and Conditions</a>
-                <a href="#">FAQ</a>
-            </div>
-            <div>
-                <h3>Contact Us</h3>
-                <p>Purok 1, Barangay Makiling, City of Calamba, Laguna</p>
-                <p>(049) 123-4567</p>
-                <p>makiling.barangay@gmail.com</p>
-                <p>Monday - Friday<br>8:00 AM - 5:00 PM</p>
-            </div>
-        </div>
-        <div class="copyright">
-            &copy; 2026 Barangay Makiling. All rights reserved.
-        </div>
-    </footer>
+    <?php include '../includes/footer.php'; ?>
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
